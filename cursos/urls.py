3fe18a85b23e192ca72768c9f9d53e1f91de0abe
Vinicius Guilherme Batista <62727555+Vini-Dev-Py@users.noreply.github.com>
2021-02-03 15:02:58 -0300

    Finalizando a API

--- cursos/urls.py
+++ cursos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import CursoAPIView, AvaliacaoAPIView
 
-urlpattenrs = [
+urlpatterns = [
     path('cursos/', CursoAPIView.as_view(), name='cursos'),
     path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,6 @@
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -14,6 +14,13 @@ class CursoAPIView(APIView):
 
         return Response(serializer.data)
 
+    def post(self, request): 
+
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class AvaliacaoAPIView(APIView):
 
     def get(self, request):
@@ -22,3 +29,10 @@ class AvaliacaoAPIView(APIView):
         serializer = AvaliacaoSerializer(avaliacaoes, many=True)
 
         return Response(serializer.data)
+
+    def post(self, request): 
+
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
